3eae3c3bb416e2d7ee9da17a42e9973fa03844af
bruno-c-p <dev@brunocardozo.com.br>
2025-05-07 19:47:40 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express"
+import type {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols"
+
+export const routeAdapter = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+    }
+    const httpResponse: HttpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
 import type { Router } from "express"
+import { routeAdapter } from "../adapters/express-route-adapter"
+import { makeSignUpController } from "../factories/signup"
 
 export default (router: Router): void => {
-  router.post("/signup", (req, res) => {
-    res.json({ message: "signup" })
-  })
+  router.post("/signup", routeAdapter(makeSignUpController()))
 }
