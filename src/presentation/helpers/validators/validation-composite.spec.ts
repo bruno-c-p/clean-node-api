0257ccdfdaa775f6d846383ab4b76389dcdcfeb2
bruno-c-p <dev@brunocardozo.com.br>
2025-06-22 23:12:05 -0300

    refactor: add validator stub to sut creation

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -5,20 +5,34 @@ import { ValidatorComposite } from "./validator-composite"
 const makeValidatorStub = (): Validator => {
   class ValidatorStub implements Validator {
     validate(input: any): Error {
-      return new MissingParamError("any_field")
+      return null
     }
   }
 
   return new ValidatorStub()
 }
 
-const makeSut = (): ValidatorComposite => {
-  return new ValidatorComposite([makeValidatorStub()])
+type SutTypes = {
+  sut: ValidatorComposite
+  validatorStub: Validator
+}
+
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidatorStub()
+  const sut = new ValidatorComposite([validatorStub])
+
+  return {
+    sut,
+    validatorStub,
+  }
 }
 
 describe("ValidatorComposite", () => {
   it("should return an error if the validation fails", () => {
-    const sut = makeSut()
+    const { sut, validatorStub } = makeSut()
+    jest
+      .spyOn(validatorStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("any_field"))
     const error = sut.validate({ any_field: "any_value" })
     expect(error).toEqual(new MissingParamError("any_field"))
   })
