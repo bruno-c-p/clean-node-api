3491d2141283881750543e47bb5ab5d4fcb27830
bruno-c-p <dev@brunocardozo.com.br>
2025-06-24 09:45:40 -0300

    fix: allow api to run locally or within a container

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
   PORT: process.env.PORT || 3000,
   MONGO_URL:
-    process.env.MONGO_URL ||
-    "mongodb://mongo-clean-node-api:27017/clean-node-api",
+    process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
 }
