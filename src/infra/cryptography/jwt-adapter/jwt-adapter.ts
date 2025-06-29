0480646d20c64d61c6fbdf9a7eaa2d5f24ce277a
bruno-c-p <dev@brunocardozo.com.br>
2025-06-29 18:11:58 -0300

    feat: ensure JwtAdapter returns a token when sign succeds

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -5,7 +5,6 @@ export class JwtAdapter implements Encrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(value: string): Promise<string> {
-    await jwt.sign({ id: value }, this.secret)
-    return Promise.resolve(null)
+    return jwt.sign({ id: value }, this.secret)
   }
 }
