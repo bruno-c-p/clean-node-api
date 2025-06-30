c4bb4b56ef2b284b096df40deec6480ce665ba76
bruno-c-p <dev@brunocardozo.com.br>
2025-06-29 22:35:10 -0300

    refactor: rename update with updateAccessToken

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -52,7 +52,7 @@ const makeEncrypterStub = (): Encrypter => {
 
 const makeUpdateAccessTokenRepositoryStub = (): UpdateAccessTokenRepository => {
   class UpdateAccessTokenRepositoryStub implements UpdateAccessTokenRepository {
-    async update(id: string, token: string): Promise<void> {
+    async updateAccessToken(id: string, token: string): Promise<void> {
       return new Promise(resolve => resolve())
     }
   }
@@ -171,7 +171,10 @@ describe("DbAuthentication UseCase", () => {
 
   it("should call UpdateAccessTokenRepository with correct values", async () => {
     const { sut, updateAccessTokenRepositoryStub } = makeSut()
-    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, "update")
+    const updateSpy = jest.spyOn(
+      updateAccessTokenRepositoryStub,
+      "updateAccessToken"
+    )
     await sut.auth(makeFakeAuthentication())
     expect(updateSpy).toHaveBeenCalledWith("any_id", "any_token")
   })
@@ -179,7 +182,7 @@ describe("DbAuthentication UseCase", () => {
   it("should throw if UpdateAccessTokenRepository throws", async () => {
     const { sut, updateAccessTokenRepositoryStub } = makeSut()
     jest
-      .spyOn(updateAccessTokenRepositoryStub, "update")
+      .spyOn(updateAccessTokenRepositoryStub, "updateAccessToken")
       .mockImplementationOnce(() => {
         throw new Error()
       })
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -35,7 +35,10 @@ export class DbAuthentication implements Authentication {
 
     const accessToken = await this.tokenGenerator.encrypt(account.id)
 
-    await this.updateAccessTokenRepository.update(account.id, accessToken)
+    await this.updateAccessTokenRepository.updateAccessToken(
+      account.id,
+      accessToken
+    )
 
     return accessToken
   }
